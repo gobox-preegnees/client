ef3d455034c21bca65c81748b41041be308d9267
gobox-preegnees <secrrb1@gmail.com>
2023-01-11 18:04:47 +0300

    синглтон для получения данных из токена

---
+++ internal/utils/jwt_parser/jwt_parser.go
@@ -0,0 +1,41 @@
+package jwtparser
+
+import (
+	"sync"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/sirupsen/logrus"
+)
+
+type identifire struct {
+	// http(S)://host:port/events?stream=<stream_id>
+	AddrSSE string
+	ClientID      string
+}
+
+var once sync.Once
+var i identifire
+
+func Parse(log *logrus.Logger, jwtToken string) identifire {
+
+	once.Do(func() {
+		token, _, err := new(jwt.Parser).ParseUnverified(jwtToken, jwt.MapClaims{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		addrSSE := claims["AddrSSE"]
+		clientID := claims["ClientID"]
+		// TODO: добавить еше проверку времени работы токена 
+		if ok && clientID != nil && addrSSE != nil {
+			i = identifire{
+				AddrSSE: addrSSE.(string),
+				ClientID:      clientID.(string),
+			}
+		} else {
+			log.Fatal("invalid token")
+		}
+	})
+	return i
+}
